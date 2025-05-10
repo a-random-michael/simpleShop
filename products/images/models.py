18ab045ff19336586c9787897151673defbd79b9
michael <michael@m-hp.localdomain>
2025-05-10 10:38:32 +0300

    deleted misplaced files

--- products/images/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from products.models import Product
-
-class Image(models.Model):
-    image = models.ImageField(null=True, blank=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#    description = models.TextField(blank=True, null=True)
